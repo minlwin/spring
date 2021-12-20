8f059d8bee714ff22eb2210829a5846c7c42ec4c
minlwin <lwin.zawmin@gmail.com>
2021-12-20 10:07:19 +0630

    Update Config for ViewResolver

--- season-3/05-hello-mvc/src/main/java/com/jdc/hello/config/ServletConfig.java
+++ season-3/05-hello-mvc/src/main/java/com/jdc/hello/config/ServletConfig.java
@@ -3,19 +3,17 @@ package com.jdc.hello.config;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.jdc.hello.controller.interceptors.HelloInterceptor;
 
@@ -34,16 +32,15 @@ public class ServletConfig implements WebMvcConfigurer{
 		registry.addViewController("/").setViewName("hello");
 	}
 	
-	@Bean
-	public ViewResolver jspViewResolver() {
-		return new InternalResourceViewResolver("/views/", ".jsp");
-	}
-	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(helloInterceptor);
 	}
 	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp().prefix("/views/").suffix("jsp");
+	}
 	
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
