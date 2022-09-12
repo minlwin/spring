0bc68a8c2232df8ba9bc2ae9f185d2cde87b8009
Min Lwin <lwin.zawmin@gmail.com>
2022-09-12 17:29:57 +0630

    Change Security Version

---
+++ assignments/leave-management-system/src/main/java/com/jdc/leaves/model/security/AccountDto.java
@@ -0,0 +1,60 @@
+package com.jdc.leaves.model.security;
+
+public class AccountDto {
+
+	private int id;
+	private String name;
+	private String role;
+	private String email;
+	private String password;
+	private boolean deleted;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+}
--- assignments/leave-management-system/src/main/java/com/jdc/leaves/model/security/LeaveApplicationUserService.java
+++ assignments/leave-management-system/src/main/java/com/jdc/leaves/model/security/LeaveApplicationUserService.java
@@ -1,8 +1,44 @@
 package com.jdc.leaves.model.security;
 
-public class LeaveApplicationUserService {
+import java.util.Map;
 
-	public LeaveApplicationUserService() {
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class LeaveApplicationUserService implements UserDetailsService{
+
+	private NamedParameterJdbcTemplate template;
+	
+	public LeaveApplicationUserService(DataSource dataSource) {
+		template = new NamedParameterJdbcTemplate(dataSource);
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		var list = template.query("select * from account where email = :email", 
+				Map.of("email", username), 
+				new BeanPropertyRowMapper<>(AccountDto.class));
+		
+		if(!list.isEmpty()) {
+			var account = list.get(0);
+			
+			return User.builder()
+					.username(account.getEmail())
+					.password(account.getPassword())
+					.authorities(account.getRole())
+					.accountExpired(account.isDeleted())
+					.build();
+		}
+		
+		return null;
+	}
+
 
 }
