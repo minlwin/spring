1cd4121f3f06bdcb860b6329de555adc5b91aa69
Min Lwin <lwin.zawmin@gmail.com>
2022-12-04 15:32:37 +0630

    Commit After Advices

--- session-7/spring-aop-aspectj-style/src/main/java/com/jdc/demo/aspects/MyConcerns.java
+++ session-7/spring-aop-aspectj-style/src/main/java/com/jdc/demo/aspects/MyConcerns.java
@@ -1,5 +1,10 @@
 package com.jdc.demo.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +13,42 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConcerns {
 	
-	@Before("execution(void hello())")
+	@Before("bean(myService)")
 	void beforeLog() {
 		System.out.println("Before Execution");
+	}
+	
+	@AfterReturning("bean(myService)")
+	void afterReturning() {
+		System.out.println("Return A Value");
+	}
+	
+	@AfterThrowing("bean(myService)")
+	void afterThrowing() {
+		System.out.println("After Throwing an exception");
+	}
+	
+	@After("bean(myService)")
+	void afterAll() {
+		System.out.println("After Finally");
+	}
+	
+	@Around("bean(myService)")
+	Object aroundInvoke(ProceedingJoinPoint joinPoint) {
+		
+		Object result = null;
+		System.out.println("Around before invoke");
+		try {
+			result = joinPoint.proceed();
+			System.out.println("Around after returning");
+		} catch (Throwable e) {
+			System.out.println("Around after throwing");
+			throw new RuntimeException(e);
+		} finally {
+			System.out.println("Around finally");
+		}
+		
+		return result;
 	}
 	
 }
