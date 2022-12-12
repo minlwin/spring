c9e5cbb3fe3da6915d7f954366217821a23f6d61
Min Lwin <lwin.zawmin@gmail.com>
2022-12-13 01:51:58 +0630

    Add Spring Transaction

--- session-8/spring-tx-jdbc-start/src/main/java/com/jdc/tx/demo/model/TransferService.java
+++ session-8/spring-tx-jdbc-start/src/main/java/com/jdc/tx/demo/model/TransferService.java
@@ -1,9 +1,12 @@
 package com.jdc.tx.demo.model;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.jdc.tx.demo.model.dto.TransferForm;
 import com.jdc.tx.demo.model.dto.TransferLog;
 
 public interface TransferService {
 
+	@Transactional
 	TransferLog transfer(TransferForm form);
 }
