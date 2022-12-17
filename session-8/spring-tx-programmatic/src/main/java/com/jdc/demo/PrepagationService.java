7ff85478e970859a5bec1aaeafa1469ca7c6299f
Min Lwin <lwin.zawmin@gmail.com>
2022-12-17 17:10:58 +0630

    Update Programmatic Transactions

--- session-8/spring-tx-programmatic/src/main/java/com/jdc/demo/PrepagationService.java
+++ session-8/spring-tx-programmatic/src/main/java/com/jdc/demo/PrepagationService.java
@@ -2,6 +2,9 @@ package com.jdc.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.interceptor.TransactionAttribute;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.jdc.demo.repo.DeatilsRepository;
 import com.jdc.demo.repo.HeaderRepository;
@@ -15,24 +18,36 @@ public class PrepagationService {
 	@Autowired
 	private DeatilsRepository detailsRepository;
 	
+	private TransactionTemplate txTemplate;
+	
+	public PrepagationService(PlatformTransactionManager txManager) {
+		txTemplate = new TransactionTemplate(txManager);
+		txTemplate.setPropagationBehavior(TransactionAttribute.PROPAGATION_REQUIRED);
+		txTemplate.setIsolationLevel(TransactionAttribute.ISOLATION_READ_COMMITTED);
+		txTemplate.setTimeout(3);
+	}
+	
 	public Result save(int state, String header, String ... details) {
 		
-		// create header
-		var headerId = headerRepository.create(header);
+		return txTemplate.execute(status -> {
+			// create header
+			var headerId = txTemplate.execute(a -> headerRepository.create(header));
+			
+			if(state == 1) {
+				throw new RuntimeException();
+			}
+			
+			
+			// create details
+			var detailsIds = txTemplate.execute(a -> detailsRepository.create(headerId, details));
+			
+			if(state == 2) {
+				throw new RuntimeException();
+			}
+			
+			return new Result(headerId, detailsIds);			
+		});
 		
-		if(state == 1) {
-			throw new RuntimeException();
-		}
-		
-		
-		// create details
-		var detailsIds = detailsRepository.create(headerId, details);
-		
-		if(state == 2) {
-			throw new RuntimeException();
-		}
-		
-		return new Result(headerId, detailsIds);
 	}
 	
 }
