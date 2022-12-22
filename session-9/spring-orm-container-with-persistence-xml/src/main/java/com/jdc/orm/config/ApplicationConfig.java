d0a09bfa4726dd70739812f2e519d5e19aa4d185
Min Lwin <lwin.zawmin@gmail.com>
2022-12-22 14:22:03 +0630

    Using Persistence XML File

---
+++ session-9/spring-orm-container-with-persistence-xml/src/main/java/com/jdc/orm/config/ApplicationConfig.java
@@ -0,0 +1,43 @@
+package com.jdc.orm.config;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@ComponentScan(basePackages = "com.jdc.orm.repo")
+@EnableTransactionManagement
+public class ApplicationConfig {
+
+	@Bean
+	DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.build();
+	}
+	
+	@Bean
+	LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource) {
+		var bean = new LocalContainerEntityManagerFactoryBean();
+		bean.setDataSource(dataSource);
+		bean.setPersistenceUnitName("demo");
+		bean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		return bean;
+	}
+	
+	@Bean
+	TransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+}
--- session-9/spring-orm-container-with-persistence-xml/src/test/java/com/jdc/test/ContainerJavaConfigTest.java
+++ session-9/spring-orm-container-with-persistence-xml/src/test/java/com/jdc/test/ContainerJavaConfigTest.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import com.jdc.orm.config.ApplicationConfig;
 import com.jdc.orm.entity.Course;
 import com.jdc.orm.repo.CourseRepo;
 
+@SpringJUnitConfig(classes = ApplicationConfig.class)
 public class ContainerJavaConfigTest {
 
 	@Autowired
