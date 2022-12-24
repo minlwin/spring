dc5317668eade09dc2f97347286f5c253d545419
Min Lwin <lwin.zawmin@gmail.com>
2022-12-24 16:51:15 +0630

    Update Configuration

--- session-10/spring-data-repositories/src/main/java/com/jdc/location/config/JpaConfiguration.java
+++ session-10/spring-data-repositories/src/main/java/com/jdc/location/config/JpaConfiguration.java
@@ -15,12 +15,10 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
-@EnableTransactionManagement
 @ComponentScan(basePackages = "com.jdc.location.model")
 @EnableJpaRepositories(basePackages = "com.jdc.location.model.repo")
 public class JpaConfiguration {
