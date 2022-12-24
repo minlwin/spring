bd7599e0674ec0d0d88a5b9282441b5d366759fa
Min Lwin <lwin.zawmin@gmail.com>
2022-12-24 16:52:20 +0630

    Share Demo Project for Custom Repository Methods

---
+++ session-10/spring-data-custom-methods/src/main/java/com/jdc/location/model/entity/State.java
@@ -0,0 +1,110 @@
+package com.jdc.location.model.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "state")
+public class State implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(nullable = false, unique = true)
+	private String name;
+	@Column(nullable = false)
+	private String type;
+	@Column(nullable = false)
+	private String region;
+	@Column(nullable = false)
+	private String capital;
+	private int porpulation;
+
+	@OneToMany(
+			mappedBy = "state", 
+			cascade = {CascadeType.PERSIST, CascadeType.MERGE}, 
+			orphanRemoval = true)
+	private List<District> district;
+
+	public enum Type {
+		State("State"), Region("Region"), Union("Union Territory");
+
+		private String value;
+
+		private Type(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+
+	public int getPorpulation() {
+		return porpulation;
+	}
+
+	public void setPorpulation(int porpulation) {
+		this.porpulation = porpulation;
+	}
+
+	public List<District> getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(List<District> district) {
+		this.district = district;
+	}
+
+}
---
+++ session-10/spring-data-custom-methods/src/main/resources/load-data.sql
@@ -0,0 +1,127 @@
+insert into state (name, type, region, capital, porpulation) values('Ayeyarwady', 'Region', 'Lower', 'Pathein', 6184829);
+insert into state (name, type, region, capital, porpulation) values('Bago', 'Region', 'Lower', 'Bago', 4867373);
+insert into state (name, type, region, capital, porpulation) values('Chin', 'State', 'West', 'Hakha', 478801);
+insert into state (name, type, region, capital, porpulation) values('Kachin', 'State', 'North', 'Myitkyina', 1689441);
+insert into state (name, type, region, capital, porpulation) values('Kayah', 'State', 'East', 'Loikaw', 286627);
+insert into state (name, type, region, capital, porpulation) values('Kayin', 'State', 'South', 'Hpa-an', 1574079);
+insert into state (name, type, region, capital, porpulation) values('Magway', 'Region', 'Central', 'Magwe', 3917055);
+insert into state (name, type, region, capital, porpulation) values('Mandalay', 'Region', 'Central', 'Mandalay', 6165723);
+insert into state (name, type, region, capital, porpulation) values('Mon', 'State', 'South', 'Mawlamyine', 2054393);
+insert into state (name, type, region, capital, porpulation) values('Naypyidaw', 'Union', 'Central', 'Naypyidaw', 1160242);
+insert into state (name, type, region, capital, porpulation) values('Rakhine', 'State', 'West', 'Sittwe', 3188807);
+insert into state (name, type, region, capital, porpulation) values('Sagaing', 'Region', 'North', 'Monywa', 5325347);
+insert into state (name, type, region, capital, porpulation) values('Shan', 'State', 'East', 'Taunggyi', 5824432);
+insert into state (name, type, region, capital, porpulation) values('Tanintharyi', 'Region', 'South', 'Dawei', 1408401);
+insert into state (name, type, region, capital, porpulation) values('Yangon', 'Region', 'Lower', 'Yangon', 7360703);
+
+-- Ayeyarwady
+insert into district (name, state_id) values ('Hinthada', 1);
+insert into district (name, state_id) values ('Kyonpyaw', 1);
+insert into district (name, state_id) values ('Labutta', 1);
+insert into district (name, state_id) values ('Ma-ubin', 1);
+insert into district (name, state_id) values ('Myaungmya', 1);
+insert into district (name, state_id) values ('Myanaung', 1);
+insert into district (name, state_id) values ('Pathein', 1);
+insert into district (name, state_id) values ('Pyapon', 1);
+
+-- Bago
+insert into district (name, state_id) values ('Bago', 2);
+insert into district (name, state_id) values ('Taungoo', 2);
+insert into district (name, state_id) values ('Pyay', 2);
+insert into district (name, state_id) values ('Tharrawaddy', 2);
+
+-- Chin
+insert into district (name, state_id) values ('Falam', 3);
+insert into district (name, state_id) values ('Hakha', 3);
+insert into district (name, state_id) values ('Matupi', 3);
+insert into district (name, state_id) values ('Mindat', 3);
+
+-- Kachin
+insert into district (name, state_id) values ('Bhamo', 4);
+insert into district (name, state_id) values ('Mohnyin', 4);
+insert into district (name, state_id) values ('Myitkyina', 4);
+insert into district (name, state_id) values ('Putao', 4);
+
+-- Kayah
+insert into district (name, state_id) values ('Bawlakhe', 5);
+insert into district (name, state_id) values ('Demoso', 5);
+insert into district (name, state_id) values ('Loikaw', 5);
+insert into district (name, state_id) values ('Mese', 5);
+
+-- Kayin
+insert into district (name, state_id) values ('Hpa-an', 6);
+insert into district (name, state_id) values ('Hpapun', 6);
+insert into district (name, state_id) values ('Kawkareik', 6);
+insert into district (name, state_id) values ('Myawaddy', 6);
+
+-- Magway
+insert into district (name, state_id) values ('Gangaw', 7);
+insert into district (name, state_id) values ('Magway', 7);
+insert into district (name, state_id) values ('Minbu', 7);
+insert into district (name, state_id) values ('Pakokku', 7);
+insert into district (name, state_id) values ('Thayet', 7);
+
+-- Mandalay
+insert into district (name, state_id) values ('Kyaukse', 8);
+insert into district (name, state_id) values ('Mandalay', 8);
+insert into district (name, state_id) values ('Meiktila', 8);
+insert into district (name, state_id) values ('Myingyan', 8);
+insert into district (name, state_id) values ('Nyaung-U', 8);
+insert into district (name, state_id) values ('Pyinoolwin', 8);
+insert into district (name, state_id) values ('Yamethin', 8);
+
+-- Mon
+insert into district (name, state_id) values ('Mawlamyine', 9);
+insert into district (name, state_id) values ('Thaton', 9);
+
+-- Naypyidaw
+insert into district (name, state_id) values ('Ottara', 10);
+insert into district (name, state_id) values ('Dekkhina', 10);
+
+-- Rakhine
+insert into district (name, state_id) values ('Kyaukpyu', 11);
+insert into district (name, state_id) values ('Maungdaw', 11);
+insert into district (name, state_id) values ('Sittwe', 11);
+insert into district (name, state_id) values ('Thandwe', 11);
+insert into district (name, state_id) values ('Mrauk-U', 11);
+
+-- Sagaing
+insert into district (name, state_id) values ('Hkamti', 12);
+insert into district (name, state_id) values ('Kanbalu', 12);
+insert into district (name, state_id) values ('Kale', 12);
+insert into district (name, state_id) values ('Katha', 12);
+insert into district (name, state_id) values ('Mawlaik', 12);
+insert into district (name, state_id) values ('Monywa', 12);
+insert into district (name, state_id) values ('Sagaing', 12);
+insert into district (name, state_id) values ('Shwebo', 12);
+insert into district (name, state_id) values ('Tamu', 12);
+insert into district (name, state_id) values ('Yinmabin', 12);
+insert into district (name, state_id) values ('Kawlin', 12);
+
+-- Shan
+insert into district (name, state_id) values ('Kengtung', 13);
+insert into district (name, state_id) values ('Mong Hpayak', 13);
+insert into district (name, state_id) values ('Mong Hsat', 13);
+insert into district (name, state_id) values ('Tachileik', 13);
+insert into district (name, state_id) values ('Kunlong', 13);
+insert into district (name, state_id) values ('Kyaukme', 13);
+insert into district (name, state_id) values ('Lashio', 13);
+insert into district (name, state_id) values ('Laukkaing', 13);
+insert into district (name, state_id) values ('Mu Se', 13);
+insert into district (name, state_id) values ('Hopang', 13);
+insert into district (name, state_id) values ('Matman', 13);
+insert into district (name, state_id) values ('Mongmit', 13);
+insert into district (name, state_id) values ('Langkho', 13);
+insert into district (name, state_id) values ('Loilen', 13);
+insert into district (name, state_id) values ('Taunggyi', 13);
+
+-- Tanintharyi
+insert into district (name, state_id) values ('Dawei', 14);
+insert into district (name, state_id) values ('Kawthaung', 14);
+insert into district (name, state_id) values ('Myeik', 14);
+
+-- Yangon
+insert into district (name, state_id) values ('East Yangon', 15);
+insert into district (name, state_id) values ('North Yangon', 15);
+insert into district (name, state_id) values ('South Yangon', 15);
+insert into district (name, state_id) values ('West Yangon', 15);
