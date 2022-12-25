dd010370de71ced8b29fa257f8de103a238820a4
Min Lwin <lwin.zawmin@gmail.com>
2022-12-25 15:24:11 +0630

    Share Combination of Predicates

--- session-10/spring-data-custom-methods/src/main/java/com/jdc/location/model/repo/DistrictRepo.java
+++ session-10/spring-data-custom-methods/src/main/java/com/jdc/location/model/repo/DistrictRepo.java
@@ -1,9 +1,17 @@
 package com.jdc.location.model.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jdc.location.model.entity.District;
 
 public interface DistrictRepo extends JpaRepository<District, Integer>{
 
+	List<District> findByStateRegion(String region);
+	
+	List<District> findByNameStartingWithIgnoringCaseOrderByName(String name);
+	
+	List<District> findByStateIdAndNameStartingWithIgnoringCaseOrderByName(int stateId, String name);
+	
 }
---
+++ session-10/spring-data-custom-methods/src/test/java/com/jdc/test/DistrictRepoTest.java
@@ -0,0 +1,50 @@
+package com.jdc.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jdc.location.model.JpaConfiguration;
+import com.jdc.location.model.repo.DistrictRepo;
+
+@SpringJUnitConfig(classes = JpaConfiguration.class)
+@Sql(scripts = {
+		"/init-tables.sql",
+		"/load-data.sql"
+})
+public class DistrictRepoTest {
+
+	@Autowired
+	private DistrictRepo repo;
+	
+	@Disabled
+	@ParameterizedTest
+	@CsvSource({
+		"West,9"
+	})
+	void test_find_by_state_region(String region, int size) {
+		var result = repo.findByStateRegion(region);
+		assertThat(result, hasSize(size));
+	}
+	
+	@Disabled
+	@ParameterizedTest
+	@CsvSource("Hpa,2")
+	void test_find_by_name_starting_with(String name, int size) {
+		var result = repo.findByNameStartingWithIgnoringCaseOrderByName(name);
+		assertThat(result, hasSize(size));
+	}
+	
+	@ParameterizedTest
+	@CsvSource("13,mo,3")
+	void test_find_by_state_and_name(int stateId, String name, int size) {
+		var result = repo.findByStateIdAndNameStartingWithIgnoringCaseOrderByName(stateId, name);
+		assertThat(result, hasSize(size));
+	}
+}
