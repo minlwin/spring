ee4397d1a76d9274763d337e0cb56034a63fd933
Min Lwin <lwin.zawmin@gmail.com>
2022-12-28 13:48:02 +0630

    Remove Blank Lines

--- session-10/spring-data-projections/src/main/java/com/jdc/location/model/record/StateRecord.java
+++ session-10/spring-data-projections/src/main/java/com/jdc/location/model/record/StateRecord.java
@@ -6,8 +6,7 @@ public record StateRecord(
 		int id,
 		String name,
 		Type type,
-		String region
-		) {
+		String region) {
 	
 	public String displayName() {
 		return "%s %s".formatted(name, type);
