ce1c50ca9108a4abee4ee7923956f8e45e6112ef
Min Lwin <lwin.zawmin@gmail.com>
2023-01-02 15:18:19 +0630

    Update Matching Rules for All Properties

--- session-10/spring-data-qbe/src/test/java/com/jdc/location/SpringDataQbeApplicationTests.java
+++ session-10/spring-data-qbe/src/test/java/com/jdc/location/SpringDataQbeApplicationTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 
 import com.jdc.location.model.entity.State;
 import com.jdc.location.model.repo.StateRepo;
@@ -18,7 +20,7 @@ class SpringDataQbeApplicationTests {
 	private StateRepo repo;
 
 	@Test
-	void find_by_region() {
+	void find_by_capital_and_region() {
 		
 		// Build Probe for constraint
 		var probe = new State();
@@ -26,12 +28,30 @@ class SpringDataQbeApplicationTests {
 		probe.setCapital("Taunggyi");
 		
 		// Create Example with probe
-		var example = Example.of(probe);
+		var example = Example.of(probe, 
+				ExampleMatcher.matching().withIgnorePaths("id", "porpulation"));
 		
 		// Search
 		var list = repo.findAll(example);
 		
 		assertThat(list, hasSize(1));
+	}
+	
+	@Test
+	void find_by_name_start_ignore_case() {
+		
+		var probe = new State();
+		probe.setName("m");
+		
+		var example = Example.of(probe,
+				ExampleMatcher.matching()
+					.withIgnorePaths("id", "porpulation")
+					.withIgnoreNullValues()
+					.withIgnoreCase()
+					.withStringMatcher(StringMatcher.STARTING));
+		
+		var result = repo.findAll(example);
+		assertThat(result, hasSize(3));
 	}
 
 }
