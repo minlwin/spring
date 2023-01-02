a43fde412765cac63a444adb928fe64f6f0d86b2
Min Lwin <lwin.zawmin@gmail.com>
2023-01-02 11:41:13 +0630

    Delete Project For QBE

--- session-10/spring-data-qbe/src/main/java/com/jdc/location/model/repo/DistrictRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jdc.location.model.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jdc.location.model.entity.District;
-
-public interface DistrictRepo extends JpaRepository<District, Integer> {
-
-}
