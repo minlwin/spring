cd75cadc4cedefb5ad9081871db8e204309ecc5b
Min Lwin <lwin.zawmin@gmail.com>
2023-01-23 11:53:53 +0630

    Update Lock

--- session-10/spring-data-locks/src/test/java/com/jdc/spring/demo/SpringDataLocksApplicationTests.java
+++ session-10/spring-data-locks/src/test/java/com/jdc/spring/demo/SpringDataLocksApplicationTests.java
@@ -2,7 +2,9 @@ package com.jdc.spring.demo;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
+import org.hibernate.jpa.AvailableHints;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -13,6 +15,7 @@ import com.jdc.spring.demo.entity.Account;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PessimisticLockScope;
 
 @SpringBootTest
 class SpringDataLocksApplicationTests {
@@ -25,7 +28,10 @@ class SpringDataLocksApplicationTests {
 	@Rollback(false)
 	void contextLoads() {
 		
-		var account = em.find(Account.class, 1, LockModeType.PESSIMISTIC_WRITE);
+		var account = em.find(Account.class, 1);
+		
+		em.lock(account, LockModeType.PESSIMISTIC_WRITE, Map.of(
+				AvailableHints.HINT_SPEC_LOCK_SCOPE, PessimisticLockScope.EXTENDED));
 		
 		account.setName(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss")));
 		
