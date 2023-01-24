0acfed42e2163cd8b7b37c69a060025d0c694594
Min Lwin <lwin.zawmin@gmail.com>
2023-01-24 14:23:00 +0630

    Lock Wait Timeout

---
+++ session-10/spring-data-locks/src/test/java/com/jdc/spring/demo/LockTimeOutDemo.java
@@ -0,0 +1,34 @@
+package com.jdc.spring.demo;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jdc.spring.demo.entity.Account;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.PersistenceContext;
+
+@SpringBootTest
+public class LockTimeOutDemo {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	@Test
+	@Transactional
+	void timeOutDemo() {
+		
+		var account = em.find(Account.class, 1);
+		
+		em.lock(account, LockModeType.PESSIMISTIC_WRITE, Map.of(
+			"jakarta.persistence.lock.timeout", "-2"));
+		
+		account.setName("Time Out Name");
+		
+	}
+}
