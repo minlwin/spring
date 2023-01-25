384d6ad7e51592463a808b3fa738bfb9bbe967ed
Min Lwin <lwin.zawmin@gmail.com>
2023-01-26 03:08:10 +0630

    Commit Getting Start Spring Boots

---
+++ session-11/thymeleaf-getting-start-boots/src/main/java/com/jdc/demo/ThymeleafGettingStartBootsApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafGettingStartBootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafGettingStartBootsApplication.class, args);
+	}
+
+}
