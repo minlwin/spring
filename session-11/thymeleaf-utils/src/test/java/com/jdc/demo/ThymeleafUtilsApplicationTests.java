b0418f1b0168d386f1ca82f5c3b9110d4a9dd109
Min Lwin <lwin.zawmin@gmail.com>
2023-02-23 20:28:40 +0630

    Update Bools Utility Class

--- session-11/thymeleaf-utils/src/test/java/com/jdc/demo/ThymeleafUtilsApplicationTests.java
+++ session-11/thymeleaf-utils/src/test/java/com/jdc/demo/ThymeleafUtilsApplicationTests.java
@@ -2,12 +2,22 @@ package com.jdc.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.unbescape.uri.UriEscape;
 
 @SpringBootTest
 class ThymeleafUtilsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		var uri = "http://localhost:8080/demo;#fragment=10/test?param=120";
+		System.out.println("escapeUriPath");
+		System.out.println(UriEscape.escapeUriPath(uri));
+		System.out.println("escapeUriPathSegment");
+		System.out.println(UriEscape.escapeUriPathSegment(uri));
+		System.out.println("escapeUriFragmentId");
+		System.out.println(UriEscape.escapeUriFragmentId(uri));
+		System.out.println("escapeUriQueryParam");
+		System.out.println(UriEscape.escapeUriQueryParam(uri));
 	}
 
 }
