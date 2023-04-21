5ebe88cd2f7c68aa6c71c0c1589ea98974ba5a04
Min Lwin <lwin.zawmin@gmail.com>
2023-04-21 20:19:46 +0630

    Commit Expression Utilities

--- session-11/thymeleaf-utils/src/main/java/com/jdc/demo/ThymeleafUtilsApplication.java
+++ session-11/thymeleaf-utils/src/main/java/com/jdc/demo/ThymeleafUtilsApplication.java
@@ -1,13 +1,20 @@
 package com.jdc.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 public class ThymeleafUtilsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafUtilsApplication.class, args);
+	}
+
+	@Autowired
+	public void setThymeleafViewResolver(ThymeleafViewResolver viewResolver) {
+		viewResolver.addStaticVariable("hello", "Hello Thymeleaf Variable");
 	}
 
 }
--- session-11/thymeleaf-utils/src/main/java/com/jdc/demo/controller/HomeController.java
+++ session-11/thymeleaf-utils/src/main/java/com/jdc/demo/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.jdc.demo.controller;
 
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,43 +16,48 @@ public class HomeController {
 	String index() {
 		return "home";
 	}
-	
+
 	@GetMapping("dates")
 	String dateTime() {
 		return "dates";
 	}
-	
+
 	@GetMapping("objects")
 	String objects(ModelMap model) {
 		model.put("nullValue", null);
 		model.put("notNullValue", "This is not null");
 		return "objects";
 	}
-	
+
 	@GetMapping("bools")
 	String bools() {
 		return "bools";
 	}
-	
+
 	@GetMapping("numbers")
 	String numbers() {
 		return "numbers";
 	}
-	
+
 	@GetMapping("strings")
 	String strings(ModelMap model) {
 		model.put("escapeXML", "<hello />");
 		model.put("escapeString", "\"Hello\' \t \r \\ Java\"");
 		return "strings";
 	}
-	
+
 	@GetMapping("collections")
-	String collections() {
+	String collections(ModelMap model) {
+
+		Comparator<Integer> reverse = (o1, o2) -> o2 - o1;
+		model.put("reverseSort", reverse);
+
 		return "collections";
 	}
-	
+
 	@GetMapping("others")
-	String otherUtils() {
+	String otherUtils(ModelMap model) {
+		model.put("subjects", List.of("Java", "Spring", "Angular", "Flutter"));
 		return "others";
 	}
 }
